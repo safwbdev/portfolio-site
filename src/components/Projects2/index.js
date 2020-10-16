3ea70fc3adc441051952bcdd1c779a2c01a14175
Stefaan <safwebdev@gmail.com>
2020-10-16 11:55:08 +0800

    update projects section

--- src/components/Projects2/index.js
+++ src/components/Projects2/index.js
@@ -41,6 +41,41 @@ export const index = ({ data, getType, title, subtitle }) => {
       </div>
     );
   };
+
+  const ProjectBox = ({
+    data: { image, title, desc, skillType, github, demo },
+  }) => {
+    return (
+      <div className="portfolio-item" key={index}>
+        <div className="image-wrapper">
+          <img src={image} alt="" width="100%" height="100%" />
+        </div>
+        <div className="title-wrapper">
+          <h2>{title}</h2>
+        </div>
+        {getType === TYPE_CLIENT && (
+          <div className="desc-wrapper">
+            <p>{desc}</p>
+          </div>
+        )}
+        {getType === TYPE_PERSONAL && <GetStacks skillType={skillType} />}
+
+        <div className={`link-wrapper ${github && demo ? "two" : "one"}`}>
+          {demo && (
+            <a href={demo} target="_blank" rel="noopener noreferrer">
+              <i className="fas fa-external-link-alt"></i>{" "}
+              {getType === TYPE_PERSONAL ? PROJECT_DEMO : PROJECT_VISIT}
+            </a>
+          )}
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github"></i> {PROJECT_GITHUB}
+            </a>
+          )}
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <div className="nav-section">
@@ -53,72 +88,11 @@ export const index = ({ data, getType, title, subtitle }) => {
             <Slider {...settings}>
               {data &&
                 data.map((project, index) => {
-                  return (
-                    <div className="portfolio-item" key={index}>
-                      <div className="image-wrapper">
-                        <img
-                          src={project.image}
-                          alt=""
-                          width="100%"
-                          height="100%"
-                        />
-                      </div>
-                      <div className="title-wrapper">
-                        <h2>{project.title}</h2>
-                      </div>
-                      {getType === TYPE_CLIENT && (
-                        <div className="desc-wrapper">
-                          <p>{project.desc}</p>
-                        </div>
-                      )}
-                      {getType === TYPE_PERSONAL && (
-                        <GetStacks skillType={project.skillType} />
-                      )}
-
-                      <div
-                        className={`link-wrapper ${
-                          project.github && project.demo ? "two" : "one"
-                        }`}
-                      >
-                        {project.demo && (
-                          <a
-                            href={project.github}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <i className="fas fa-external-link-alt"></i>{" "}
-                            {getType === TYPE_PERSONAL
-                              ? PROJECT_DEMO
-                              : PROJECT_VISIT}
-                          </a>
-                        )}
-                        {project.github && (
-                          <a
-                            href={project.github}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <i className="fab fa-github"></i> {PROJECT_GITHUB}
-                          </a>
-                        )}
-                      </div>
-                    </div>
-                  );
+                  return <ProjectBox key={index} data={project} />;
                 })}
             </Slider>
           </div>
         </div>
-      </div>
-
-      {/* MODAL */}
-      <div className="portfolio-modal">
-        <div className="close-button-wrapper">
-          <button className="close-button">
-            <i className="fa fa-times"></i>
-          </button>
-        </div>
-
-        <div className="image-wrapper"></div>
       </div>
     </>
   );
